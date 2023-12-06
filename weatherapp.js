e88e3a81c4cef223796ef311550d09e20c36bfa1
Alex Whalen <awhalen1999@gmail.com>
2023-12-06 15:59:55 -0330

    weather app card format/display

--- weatherapp.js
+++ weatherapp.js
@@ -11,7 +11,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
     return `<div class="details">
                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
                     <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(
-                      2
+                      1
                     )}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
@@ -24,12 +24,12 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                 </div>`;
   } else {
     return `<li class="card">
-                    <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
+                    <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
                     <img src="https://openweathermap.org/img/wn/${
                       weatherItem.weather[0].icon
                     }@4x.png" alt="weather-icon">
                     <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(
-                      2
+                      1
                     )}°C</h6>
                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
