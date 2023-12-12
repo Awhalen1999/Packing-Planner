871f1453b7059b34b28193156b16eb9256ab5141
Alex Whalen <awhalen1999@gmail.com>
2023-12-12 15:38:14 -0330

    async await

--- weatherapp.js
+++ weatherapp.js
@@ -37,70 +37,69 @@ const createWeatherCard = (cityName, weatherItem, index) => {
   }
 };
 
-const getWeatherDetails = (cityName, latitude, longitude) => {
+const getWeatherDetails = async (cityName, latitude, longitude) => {
   const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
 
-  fetch(WEATHER_API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      const uniqueForecastDays = [];
-      const fiveDaysForecast = data.list.filter((forecast) => {
-        const forecastDate = new Date(forecast.dt_txt).getDate();
-        if (!uniqueForecastDays.includes(forecastDate)) {
-          return uniqueForecastDays.push(forecastDate);
-        }
-      });
+  try {
+    const response = await fetch(WEATHER_API_URL);
+    const data = await response.json();
 
-      cityInput.value = "";
-      currentWeatherDiv.innerHTML = "";
-      weatherCardsDiv.innerHTML = "";
-
-      fiveDaysForecast.forEach((weatherItem, index) => {
-        const html = createWeatherCard(cityName, weatherItem, index);
-        if (index === 0) {
-          currentWeatherDiv.insertAdjacentHTML("beforeend", html);
-        } else {
-          weatherCardsDiv.insertAdjacentHTML("beforeend", html);
-        }
-      });
-    })
-    .catch(() => {
-      alert("An error occurred while fetching the weather forecast!");
+    const uniqueForecastDays = [];
+    const fiveDaysForecast = data.list.filter((forecast) => {
+      const forecastDate = new Date(forecast.dt_txt).getDate();
+      if (!uniqueForecastDays.includes(forecastDate)) {
+        return uniqueForecastDays.push(forecastDate);
+      }
     });
+
+    cityInput.value = "";
+    currentWeatherDiv.innerHTML = "";
+    weatherCardsDiv.innerHTML = "";
+
+    fiveDaysForecast.forEach((weatherItem, index) => {
+      const html = createWeatherCard(cityName, weatherItem, index);
+      if (index === 0) {
+        currentWeatherDiv.insertAdjacentHTML("beforeend", html);
+      } else {
+        weatherCardsDiv.insertAdjacentHTML("beforeend", html);
+      }
+    });
+  } catch (error) {
+    alert("An error occurred while fetching the weather forecast!");
+  }
 };
 
-const getCityCoordinates = () => {
+const getCityCoordinates = async () => {
   const cityName = cityInput.value.trim();
   if (cityName === "") return;
   const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
 
-  fetch(API_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      if (!data.length) return alert(`No coordinates found for ${cityName}`);
-      const { lat, lon, name } = data[0];
-      getWeatherDetails(name, lat, lon);
-    })
-    .catch(() => {
-      alert("An error occurred while fetching the coordinates!");
-    });
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    if (!data.length) return alert(`No coordinates found for ${cityName}`);
+    const { lat, lon, name } = data[0];
+    getWeatherDetails(name, lat, lon);
+  } catch (error) {
+    alert("An error occurred while fetching the coordinates!");
+  }
 };
 
-const getUserCoordinates = () => {
+const getUserCoordinates = async () => {
   navigator.geolocation.getCurrentPosition(
-    (position) => {
+    async (position) => {
       const { latitude, longitude } = position.coords;
 
       const API_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`;
-      fetch(API_URL)
-        .then((response) => response.json())
-        .then((data) => {
-          const { name } = data[0];
-          getWeatherDetails(name, latitude, longitude);
-        })
-        .catch(() => {
-          alert("An error occurred while fetching the city name!");
-        });
+      try {
+        const response = await fetch(API_URL);
+        const data = await response.json();
+        const { name } = data[0];
+        getWeatherDetails(name, latitude, longitude);
+      } catch (error) {
+        alert("An error occurred while fetching the city name!");
+      }
     },
     (error) => {
       if (error.code === error.PERMISSION_DENIED) {
