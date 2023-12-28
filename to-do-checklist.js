5902a2495c0bdcecbdb902c90959b5420868e41b
Alex Whalen <awhalen1999@Gmail.com>
2023-12-28 17:18:52 -0330

    add item button functioning

--- to-do-checklist.js
+++ to-do-checklist.js
@@ -63,3 +63,33 @@ toDoListItems.forEach((item) => {
     window.location.href = "./to-do-checked-items.html";
   });
 });
+
+// Get the button and input elements from the DOM.
+// Get the button, input, and checklist elements from the DOM.
+let button = document.getElementById("to-do-add-item-button");
+let input = document.getElementById("to-do-input");
+let checklist = document.getElementById("to-do-checklist"); // Replace with the id of your checklist element.
+
+// Add an event listener to the button. When the button is clicked, add the inputted text to the checklist.
+button.addEventListener("click", function () {
+  // Get the inputted text.
+  let newItem = input.value.trim();
+
+  // If the inputted text is not empty, add it to the checklist.
+  if (newItem) {
+    // Get the `to-do-checked` array from local storage.
+    let checkedItems = JSON.parse(localStorage.getItem("to-do-checked")) || [];
+
+    // Add the new item to the checked items array.
+    checkedItems.push(newItem);
+
+    // Save the updated checked items array to local storage.
+    localStorage.setItem("to-do-checked", JSON.stringify(checkedItems));
+
+    // Add the new item to the checklist display.
+    checklist.innerHTML += `<li><input type="checkbox">${newItem}</li>`;
+
+    // Clear the input field.
+    input.value = "";
+  }
+});
