53c81de7eef11f4d2d64494349053e4fb5635ba6
Alex Whalen <awhalen1999@Gmail.com>
2024-01-02 13:51:32 -0330

    added js to handle nav/checklist link

---
+++ nav.js
@@ -0,0 +1,20 @@
+const gender = localStorage.getItem("gender");
+const climate = localStorage.getItem("climate");
+const navContainer = document.getElementById("nav-container");
+
+const checklistLink =
+  !gender || !climate ? "./no-checklist.html" : "./checklist.html";
+
+const navHTML = `
+      <nav class="main-nav">
+        <ul>
+          <li><a href="./index.html">Home</a></li>
+          <li><a href="${checklistLink}">Checklist</a></li>
+          <li><a href="./to-do-checklist.html">To-Do Checklist</a></li>
+          <li><a href="./weather-app.html">Check the weather</a></li>
+          <li><a href="./about.html">About</a></li>
+          <li><a href="./contact.html">Contact Me</a></li>
+        </ul>
+      </nav>`;
+
+navContainer.innerHTML = navHTML;
